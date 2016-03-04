37125299789fd42e8ac71fcb5fbdf9a3392a0ed9
Bruce Failor <bruce.failor@gmail.com>
2016-03-04 20:18:01 +0000

    stub http requests

--- app/services/food.rb
+++ app/services/food.rb
@@ -1,14 +1,47 @@
 class Food
-  attr_reader :errors
-  AUTH = '&api_key=' + ENV['DATA_DOT_GOV_API_KEY']
-  BASE = 'http://api.nal.usda.gov/ndb/reports/?'
+  attr_reader :errors, :results
+  UNITS = 'string,,g,,USD,,g,,g,,calorie,,calorie,,calorie,,calorie,,calorie' \
+          ',,g,,g,,g,,g,,g,,,,g,,g,,g,,g,,g,,mg,,mg,,g,,g,,IU,,mg,,mcg,,mg,,' \
+          'mcg,,mg,,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg' \
+          ',,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,g,,g,,g,,mg,,mg,,string'.split(',,')
+  SOURCES = \
+  'name,,dft,,food price,,vegetable grams,,fruit grams,,208,,calc carb Cal,,' \
+  'calc fat Cal,,calc protein Cal,,calc alcohol Cal,,205,,291,,209,,269,,204' \
+  ',,606,,645,,646,,605,,693,,695,,calc omega-3,,calc omega-6,,203,,510,,318' \
+  ',,401,,328,,323,,430,,404,,405,,406,,415,,417,,418,,410,,421,,454,,301,,3' \
+  '03,,304,,305,,306,,307,,309,,312,,315,,317,,313,,601,,636,,221,,255,,207,' \
+  ',262,,263,,ndbno'.split(',,')
+  NAMES = \
+  'food,,base mass,,price,,vegetable mass,,fruit mass,,energy (E),,carbohydr' \
+  'ate E,,fat E,,protein E,,alcohol E,,total carbohydrates,,fiber,,starch,,s' \
+  'ugars,,total fat,,saturated fat,,monounsaturated fat,,polyunsaturated fat' \
+  ',,total trans fat,,trans-monoeonic,,trans-polyenoic,,omega-3,,omega-6,,pr' \
+  'otein,,tryptophan,,vitamin A,,vitamin C,,vitamin D,,vitamin E,,vitamin K,' \
+  ',thiamine,,riboflavin,,niacin,,vitamin B6,,folate,,vitamin B12,,pantothen' \
+  'ic acid,,choline,,betaine,,calcium,,iron,,magnesium,,phosphorus,,potassiu' \
+  'm,,sodium,,zinc,,copper,,manganese,,selenium,,fluoride,,cholesterol,,phyt' \
+  'osterols,,alcohol,,water,,ash,,caffeine,,theobromine,,ndb food number'
+    .split(',,')
 
   def initialize(args = {})
     @code = args[:code]
     assign_results unless errors_exist
   end
 
+  private
+  AUTH = '&api_key=' + ENV['DATA_DOT_GOV_API_KEY']
+  BASE = 'http://api.nal.usda.gov/ndb/reports/?'
+
   def assign_results
+ #   calc = Food::Calculator.new(sources: SOURCES,
+ #                               food: body['report']['food']).calculation
+    @results = {
+      units: \
+        'string,,g,,USD,,g,,g,,calorie,,calorie,,calorie,,calorie,,calorie' \
+        ',,g,,g,,g,,g,,g,,,,g,,g,,g,,g,,g,,mg,,mg,,g,,g,,IU,,mg,,mcg,,mg,,' \
+        'mcg,,mg,,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg' \
+        ',,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,g,,g,,g,,mg,,mg,,string'.split(',,')
+    }
   end
 
   def errors_exist
@@ -28,8 +61,8 @@ class Food
 
   def url
     @url ||= begin
-               data = URI.encode_www_form(ndbno: @code, type: 's',
-                                          format: 'json')
+               data = URI
+                 .encode_www_form(ndbno: @code, type: 's', format: 'json')
                URI.parse(BASE + data + AUTH)
              end
   end
--- spec/services/food_spec.rb
+++ spec/services/food_spec.rb
@@ -1,26 +1,65 @@
 require 'rails_helper'
 
 describe Food do
-  let(:klass) { Food }
-  it 'responds to :new' do
-    expect(klass.new.class).to eq klass
-  end
+  let(:fixtures_dir) { File.join(Rails.root, 'spec', 'fixtures') }
+  let(:sad_response) { YAML
+      .load(File.open(File.join(fixtures_dir, 'sad_response.yml')))}
+  let(:happy_response) { YAML
+      .load(File.open(File.join(fixtures_dir, 'happy_response.yml')))}
+  let(:http_double) { double }
 
   context 'invalid food number' do
     let(:invalid_code) {'00000'}
 
     it 'reports errors' do
+      expect(Net::HTTP).to receive(:new).and_return(http_double)
+      expect(http_double).to receive(:request).and_return(sad_response)
       food = Food.new(code: invalid_code)
       expect(food.errors).to eq "No data for this ndbno: #{invalid_code}"
     end
   end
 
   context 'valid food number' do
-    let(:valid_code) {'11356'}
+    let(:valid_code) {'11356'} # baked russet potatoes, flesh and skin
+    let(:food) { Food.new(code: valid_code) }
 
     it 'reports no errors' do
-      food = Food.new(code: valid_code)
+      expect(Net::HTTP).to receive(:new).and_return(http_double)
+      expect(http_double).to receive(:request).and_return(happy_response)
       expect(food.errors).to eq nil
     end
+
+    it 'returns results hash' do
+      expect(Net::HTTP).to receive(:new).and_return(http_double)
+      expect(http_double).to receive(:request).and_return(happy_response)
+      expect(food.results).not_to eq nil
+    end
+
+    describe 'results hash' do
+      it 'has the correct units' do
+        # confirm the number of elements first, then the actual values
+        expect(food.results[:units].count).to eq Food::UNITS.count
+        expect(food.results[:units].count).to eq Food::SOURCES.count
+        expect(food.results[:units].count).to eq Food::NAMES.count
+      end
+      
+      
+=begin
+I have the header values for names, units, and sources.  I can compare that
+with the calculated names and units.  And also calculate the values.
+I can compare the values for a few foods on nutritiondata.self.com
+
+Once the values look right, I need to get a controller working with the new and
+show actions -- :new will allow the input of the food code and will act as the
+root page for the app; :show will display the results so they can be cut and
+pasted into the spreadsheet program.  I am planning to join all the elements
+with tab characters to that they will paste directly into the cells in the 
+spreadsheet.  Joining and then displaying the tabs may be tricky.  I thought 
+that I would use a form tag and prefill it with the values of the strings to be
+cut and pasted.
+=end
+    end
+    
+    
   end
 end
