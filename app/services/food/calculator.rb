37197ce1853c487624c221f475ba3c1c8ef8c284
Bruce Failor <bruce.failor@gmail.com>
2016-03-05 22:30:38 +0000

    add food nutrient calculator

---
+++ app/services/food/calculator.rb
@@ -0,0 +1,155 @@
+##
+# TODO: update this class comment
+# This class pulls specific nutrition data for a food that is present in the
+# United States Department of Agriculture, Agricultural Research Service,
+# National Nutrient Database (https://ndb.nal.usda.gov/ndb/doc/index)
+class Food::Calculator
+  attr_reader :results
+
+  def initialize(args = {})
+    @sources = args[:sources]
+    @food    = args[:food]
+    @results = {}
+    @sources.each do |s|
+      if /\d{3}/.match s
+        parse_nutrient(s)
+      else
+        send(s.to_sym) if respond_to? s.to_sym
+      end
+    end
+  end
+
+  def parse_nutrient(nutrient_id)
+    n = nutrient_hash[nutrient_id]
+    if n
+      @results[nutrient_id] =
+        { name: n['name'], unit: n['unit'], value: n['value'] }
+    else
+      nil
+    end
+    'ok'
+  end
+
+  def nutrient_hash
+    @nh ||= begin
+              nhash = {}
+              @food['nutrients'].each { |n| nhash[n['nutrient_id']] = n }
+              nhash
+            end
+  end
+
+  class << self
+    def define(name, &block) # see ActiveSupport::Testing::Declarative#test
+      name_sym = name.to_sym
+      fail "#{name_sym} already defined in #{self}" if method_defined? name_sym
+      return define_method(name_sym, &block) if block_given?
+      define_method(name_sym) { flunk "No implementation for #{name}" }
+    end
+  end
+
+  define('name')            { foo(key: __callee__) }
+  define('dft')             { foo(value: '100', key: __callee__) }
+  define('ndbno')           { foo(key: __callee__) }
+  define('calc omega-3')    { foo(value: omega_3_value, key: __callee__) }
+  define('calc omega-6')    { foo(value: omega_6_value, key: __callee__) }
+  define('calc fat Cal')    { foo(value: '100', key: __callee__) }
+  define('food price')      { foo(value: 'user-specified', key: __callee__) }
+  define('vegetable grams') { foo(value: 'user-specified', key: __callee__) }
+  define('fruit grams')     { foo(value: 'user-specified', key: __callee__) }
+
+  def omega_3_value
+    sum = 0.0
+    %w(851 627 852 629 857 631 621).each do |key|
+      sum += nutrient_hash[key] ? nutrient_hash[key]['value'].to_f*1000 : 0.0
+    end
+    sum.to_s # assume nutrient units are grams
+  end
+
+  def omega_6_value
+    sum = 0.0
+    %w(675 685 672 853 855 858).each do |key|
+      p nutrient_hash[key]['unit'] if nutrient_hash[key]
+      sum += nutrient_hash[key] ? nutrient_hash[key]['value'].to_f*1000 : 0.0
+    end
+    sum.to_s # assume nutrient units are grams
+  end
+
+   def foo(args = {})
+    key = args[:key].to_s
+    indx = Food::SOURCES.index(key)
+    @results[key] = { name: Food::NAMES[indx],
+      unit: Food::UNITS[indx], value: args[:value] || @food[key] }
+  end
+end
+
+=begin
+  attr_reader :errors, :results
+
+  UNITS = 'string,,g,,USD,,g,,g,,calorie,,calorie,,calorie,,calorie,,calorie' \
+          ',,g,,g,,g,,g,,g,,,,g,,g,,g,,g,,g,,mg,,mg,,g,,g,,IU,,mg,,mcg,,mg,,' \
+          'mcg,,mg,,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg' \
+          ',,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,g,,g,,g,,mg,,mg,,string'.split(',,')
+  SOURCES = \
+  'name,,dft,,food price,,vegetable grams,,fruit grams,,208,,calc carb Cal,,' \
+  'calc fat Cal,,calc protein Cal,,calc alcohol Cal,,205,,291,,209,,269,,204' \
+  ',,606,,645,,646,,605,,693,,695,,calc omega-3,,calc omega-6,,203,,510,,318' \
+  ',,401,,328,,323,,430,,404,,405,,406,,415,,417,,418,,410,,421,,454,,301,,3' \
+  '03,,304,,305,,306,,307,,309,,312,,315,,317,,313,,601,,636,,221,,255,,207,' \
+  ',262,,263,,ndbno'.split(',,')
+  NAMES = \
+  'food,,base mass,,price,,vegetable mass,,fruit mass,,energy (E),,carbohydr' \
+  'ate E,,fat E,,protein E,,alcohol E,,total carbohydrates,,fiber,,starch,,s' \
+  'ugars,,total fat,,saturated fat,,monounsaturated fat,,polyunsaturated fat' \
+  ',,total trans fat,,trans-monoeonic,,trans-polyenoic,,omega-3,,omega-6,,pr' \
+  'otein,,tryptophan,,vitamin A,,vitamin C,,vitamin D,,vitamin E,,vitamin K,' \
+  ',thiamine,,riboflavin,,niacin,,vitamin B6,,folate,,vitamin B12,,pantothen' \
+  'ic acid,,choline,,betaine,,calcium,,iron,,magnesium,,phosphorus,,potassiu' \
+  'm,,sodium,,zinc,,copper,,manganese,,selenium,,fluoride,,cholesterol,,phyt' \
+  'osterols,,alcohol,,water,,ash,,caffeine,,theobromine,,ndb food number'
+    .split(',,')
+
+  def initialize(args = {})
+    @num = args[:number]
+    assign_results unless errors_exist
+  end
+
+  private
+
+  AUTH = "&api_key=#{ENV['DATA_DOT_GOV_API_KEY']}"
+  BASE = 'http://api.nal.usda.gov/ndb/reports/?'
+
+  def assign_results
+    # calc = Food::Calculator.new(sources: SOURCES,
+    #   food: body['report']['food']).calculation
+    @results = {
+      units: \
+        'string,,g,,USD,,g,,g,,calorie,,calorie,,calorie,,calorie,,calorie' \
+        ',,g,,g,,g,,g,,g,,,,g,,g,,g,,g,,g,,mg,,mg,,g,,g,,IU,,mg,,mcg,,mg,,' \
+        'mcg,,mg,,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg,,mg' \
+        ',,mg,,mg,,mg,,mcg,,mcg,,mg,,mg,,g,,g,,g,,mg,,mg,,string'.split(',,')
+    }
+  end
+
+  def errors_exist
+    @errors = "#{body['errors']['error'].first['message']}: #{@num}" \
+      if body['errors']
+    @errors ? true : false
+  end
+
+  def body
+    @body ||= begin
+                http = Net::HTTP.new(url.host, url.port)
+                response = http.request(Net::HTTP::Get.new(url))
+                JSON.parse(response.read_body)
+              end
+  end
+
+  def url
+    @url ||= begin
+               data = URI
+                 .encode_www_form(ndbno: @num, type: 's', format: 'json')
+               URI.parse(BASE + data + AUTH)
+             end
+  end
+end
+=end
---
+++ spec/services/food/calculator_spec.rb
@@ -0,0 +1,127 @@
+require 'rails_helper'
+
+describe Food::Calculator do
+  let(:fixtures_dir) { File.join(Rails.root, 'spec', 'fixtures') }
+  let(:happy_response) { YAML
+      .load(File.open(File.join(fixtures_dir, 'happy_response.yml')))}
+  let(:fd) { JSON.parse(happy_response.read_body)['report']['food'] }
+  let(:kalculator) { Food::Calculator.new(sources: Food::SOURCES, food: fd) }
+
+  it 'returns the expected results for nutrients' do
+    expect(kalculator.results['208'])
+             .to eq( { name: "Energy", unit: "kcal", value: "97" })
+  end
+
+  it 'returns the expected results for food name' do
+    expect(kalculator.results['name'])
+      .to eq({ name: "food",
+               unit: "string",
+               value: "Potatoes, Russet, flesh and skin, baked" })
+  end
+
+  it 'returns the expected results for ndbno' do
+    expect(kalculator.results['ndbno'])
+      .to eq({ name: 'ndb food number',
+               unit: 'string',
+               value: '11356' })
+  end
+
+  it 'returns the expected results for dft' do
+    expect(kalculator.results['dft'])
+      .to eq({ name: 'base mass',
+               unit: 'g',
+               value: '100' })
+  end
+
+  it 'returns the expected results for omega-3' do
+    expect(kalculator.results['calc omega-3'])
+      .to eq({ name: 'omega-3',
+               unit: 'mg',
+               value: '0.0' })
+  end
+
+  it 'returns the expected results for omega-6' do
+    expect(kalculator.results['calc omega-6'])
+      .to eq({ name: 'omega-6',
+               unit: 'mg',
+               value: '0.0' })
+  end
+
+  it 'returns the expected results for calories from fat' do
+    expect(kalculator.results['calc fat Cal'])
+      .to eq({ name: 'fat E',
+               unit: 'calorie',
+               value: '100' }) # TODO: fix value
+  end
+
+  it 'returns the expected results for price' do
+    expect(kalculator.results['food price'])
+      .to eq({ name: 'price',
+               unit: 'USD',
+               value: 'user-specified' })
+  end
+
+ it 'returns the expected results for vegetable mass' do
+    expect(kalculator.results['vegetable grams'])
+      .to eq({ name: 'vegetable mass',
+               unit: 'g',
+               value: 'user-specified' })
+  end
+
+  it 'returns the expected results for fruit mass' do
+    expect(kalculator.results['fruit grams'])
+      .to eq({ name: 'fruit mass',
+               unit: 'g',
+               value: 'user-specified' })
+  end
+
+end
+
+=begin
+  context 'valid food number' do
+    let(:valid_number) {'11356'} # baked russet potatoes, flesh and skin
+    let(:food) { Food.new(number: valid_number) }
+
+    it 'reports no errors' do
+      expect(Net::HTTP).to receive(:new).and_return(http_double)
+      expect(http_double).to receive(:request).and_return(happy_response)
+      expect(food.errors).to eq nil
+    end
+
+    it 'returns results hash' do
+      expect(Net::HTTP).to receive(:new).and_return(http_double)
+      expect(http_double).to receive(:request).and_return(happy_response)
+      expect(food.results).not_to eq nil
+    end
+
+    describe 'results hash' do
+      it 'has the correct units' do
+        # confirm the number of elements first, then the actual values
+        expect(food.results[:units].count).to eq Food::UNITS.count
+        expect(food.results[:units].count).to eq Food::SOURCES.count
+        expect(food.results[:units].count).to eq Food::NAMES.count
+      end
+      
+      
+=end
+=begin
+I have the header values for names, units, and sources.  I can compare that
+with the calculated names and units.  And also calculate the values.
+I can compare the values for a few foods on nutritiondata.self.com
+
+Once the values look right, I need to get a controller working with the new and
+show actions -- :new will allow the input of the food number and will act as the
+root page for the app; :show will display the results so they can be cut and
+pasted into the spreadsheet program.  I am planning to join all the elements
+with tab characters to that they will paste directly into the cells in the 
+spreadsheet.  Joining and then displaying the tabs may be tricky.  I thought 
+that I would use a form tag and prefill it with the values of the strings to be
+cut and pasted.
+=end
+=begin
+    end
+    
+    
+  end
+end
+=end
